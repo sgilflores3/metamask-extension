1687d3290da69bbff461b9b48806fa449284d261
BelfordZ <belfordz66@gmail.com>
2023-11-17 13:19:53 -0800

    wip

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4592,12 +4592,15 @@ export default class MetamaskController extends EventEmitter {
       );
     }
 
-    let proxyProviderForDomain = provider;
+    let proxyClient = {
+      provider,
+      blockTracker
+    };
     if (this.preferencesController.getUseRequestQueue()) {
-      proxyProviderForDomain =
+      proxyClient =
         this.selectedNetworkController.getProviderAndBlockTracker(
           origin,
-        ).provider;
+        );
     }
 
     const requestQueueMiddleware = createQueuedRequestMiddleware({
@@ -4610,13 +4613,13 @@ export default class MetamaskController extends EventEmitter {
     engine.push(requestQueueMiddleware);
 
     // create filter polyfill middleware
-    const filterMiddleware = createFilterMiddleware({ provider, blockTracker });
+    const filterMiddleware = createFilterMiddleware(proxyClient);
+    console.log('selectedNetworkClientId:', selectedNetworkClientIdForDomain);
+    console.log('proxyClient:', proxyClient);
+    debugger;
 
     // create subscription polyfill middleware
-    const subscriptionManager = createSubscriptionManager({
-      provider,
-      blockTracker,
-    });
+    const subscriptionManager = createSubscriptionManager(proxyClient);
     subscriptionManager.events.on('notification', (message) =>
       engine.emit('notification', message),
     );
@@ -4892,7 +4895,7 @@ export default class MetamaskController extends EventEmitter {
 
     engine.push(this.metamaskMiddleware);
 
-    engine.push(providerAsMiddleware(proxyProviderForDomain));
+    engine.push(providerAsMiddleware(proxyClient.provider));
 
     return engine;
   }
