7c11b2c2b9f15904c7ff8380a0672343b531d0f2
Zachary Belford <belfordz66@gmail.com>
2024-01-16 09:42:18 -0800

    looking good

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -490,8 +490,6 @@ export default class MetamaskController extends EventEmitter {
       }),
     });
 
-    console.log(this.selectedNetworkController.state);
-
     if (!this.selectedNetworkController.getNetworkClientIdForDomain('metamask')) {
       this.selectedNetworkController.setNetworkClientIdForMetamask(this.networkController.state.selectedNetworkClientId);
     }
@@ -516,14 +514,11 @@ export default class MetamaskController extends EventEmitter {
       networkConfigurations: this.networkController.state.networkConfigurations,
     });
 
-    this.preferencesController.subscribe(() => {
-      debugger;
-      console.log(this.selectedNetworkController.setPerDappNetwork)
-    })
-
- //    this.selectedNetworkController.update((state) => {
-   //   state.perDomainNetwork = this.preferencesController.getUseRequestQueue();
-   // });
+    this.preferencesController.store.subscribe(({useRequestQueue}) => {
+      if (useRequestQueue !== this.selectedNetworkController.state.perDomainNetwork) {
+        this.selectedNetworkController.setPerDomainNetwork(useRequestQueue);
+      }
+    });
 
     this.assetsContractController = new AssetsContractController(
       {
